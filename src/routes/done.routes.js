0fc8dff53038fe13f1d120a829943d9b3ae36a39
Mugilarasan <mugilarasan@M2P-LAP-1078.local>
2023-03-20 16:52:42 +0530

    todo routes uodate

--- src/routes/done.routes.js
+++ src/routes/done.routes.js
@@ -5,7 +5,7 @@ const controller = require("../controller/done.controller");
 router.get("/list", controller.list);
 router.get("/get/:id", controller.get);
 router.post("/create", controller.create);
-router.put("/update/:id", controller.update);
+router.patch("/update/:id", controller.update);
 router.delete("/delete/:id", controller.delete);
 
 module.exports = router;
